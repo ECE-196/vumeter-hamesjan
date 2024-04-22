297080f26298cd033a22b81e747c75c509910677
hamesjan <jhan31903@gmail.com>
2024-04-22 16:05:35 -0700

    added everything

---
+++ CHANGELOG.md
@@ -0,0 +1,8 @@
+4/15/24
+
+Completed VUMETER,
+need to finish enclosure.
+
+4/22/24
+Finished enclosure on onshape
+printed out the enclosure and finished assignment.
--- main.py
+++ main.py
@@ -11,9 +11,16 @@ status.direction = Direction.OUTPUT
 
 led_pins = [
     board.IO21,
-    board.IO26, # type: ignore
+    board.IO26,  # type: ignore
     board.IO47,
-    # do the rest...
+    board.IO33,  # type: ignore
+    board.IO34,  # type: ignore
+    board.IO48,
+    board.IO35,
+    board.IO36,
+    board.IO37,
+    board.IO38,
+    board.IO39
 ]
 
 leds = [DigitalInOut(pin) for pin in led_pins]
@@ -26,11 +33,27 @@ while True:
     volume = microphone.value
 
     print(volume)
-
-    leds[0].value = not leds[0].value
-    leds[1].value = not leds[0].value
-
-    sleep(1)
+    if (volume > 40000):
+        for i in range(11):
+            leds[i].value = 1
+    elif (volume > 35000):
+        for i in range(9):
+            leds[i].value = 1
+    elif (volume > 30000):
+        for i in range(7):
+            leds[i].value = 1
+    elif (volume > 25000):
+        for i in range(5):
+            leds[i].value = 1
+    elif (volume > 20000):
+        for i in range(3):
+            leds[i].value = 1
+    elif (volume > 15000):
+        for i in range(1):
+            leds[i].value = 1
+    else:
+        for i in range(11):
+            leds[i].value = 0
 
     # instead of blinking,
     # how can you make the LEDs
